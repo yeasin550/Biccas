5585c07e0028c661cec424292d43d4fbe81769bf
yeasin550 <yeasin51570@gmail.com>
2023-11-23 23:24:37 +0600

    home page complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
               href="https://themesberg.com/product/tailwind-css/landing-page"
               className="text-white bg-[#54BD95] hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
             >
-              Download
+              Register
             </a>
           </div>
         </div>
